183713c5f9071cac26bbc58927f680f3fa701483
ucesfranp <ucesfranpollet@gmail.com>
2026-07-22 18:30:20 -0300

    Fix test

--- __tests__/React_Testing_Library/PagoItem.test.tsx
+++ __tests__/React_Testing_Library/PagoItem.test.tsx
@@ -33,11 +33,11 @@ it('llama onTogglePaid al presionar el toggle', async () => {
 
 it('llama onEliminar al presionar X', async () => {
     const { getByText } = await renderItem();
-    fireEvent.press(getByText('❌'));
+    fireEvent.press(getByText('🗑️'));
     expect(mockEliminar).toHaveBeenCalledWith(1);
 });
 
-it('muestra [X] si pagado', async () => {
+it('muestra ✅ si pagado', async () => {
     const { getByText } = await renderItem({ ...pago, pagado: true, pagoFecha: new Date().toISOString() });
-    expect(getByText('[X]')).toBeTruthy();
+    expect(getByText('✅')).toBeTruthy();
 });
